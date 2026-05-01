8510dcf6248f8bf70272ee7d4f8ef0086453b3d4
William Chen <exanite78012@gmail.com>
2026-05-01 18:02:17 -0400

    Generate on Linux

--- sources/Vulkan/Vulkan/Vulkan/PhysicalDevicePresentMeteringFeaturesNV.gen.cs
+++ sources/Vulkan/Vulkan/Vulkan/PhysicalDevicePresentMeteringFeaturesNV.gen.cs
@@ -21,7 +21,8 @@ public unsafe partial struct PhysicalDevicePresentMeteringFeaturesNV
             "VK_NV_present_metering+VK_VERSION_1_1",
         ]
     )]
-    public StructureType SType;
+    public StructureType SType =
+        StructureType.VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PRESENT_METERING_FEATURES_NV;
 
     [NativeName("pNext")]
     [SupportedApiProfile(
@@ -44,4 +45,14 @@ public unsafe partial struct PhysicalDevicePresentMeteringFeaturesNV
         ]
     )]
     public uint PresentMetering;
+
+    [SupportedApiProfile(
+        "vulkan",
+        ["VK_NV_present_metering"],
+        ImpliesSets = [
+            "VK_NV_present_metering+VK_KHR_get_physical_device_properties2",
+            "VK_NV_present_metering+VK_VERSION_1_1",
+        ]
+    )]
+    public PhysicalDevicePresentMeteringFeaturesNV() { }
 }
